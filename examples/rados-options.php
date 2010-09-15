3f8cfa09934df3c5152cc6a4be0277d452432501
Wido den Hollander <wido@widodh.nl>
2010-09-15 13:39:34 +0200

    Fix parse error

--- examples/rados-options.php
+++ examples/rados-options.php
@@ -3,10 +3,9 @@
 $options = array(
     'config_file' => '/path/to/ceph.conf', // -c
     'monitor_ip' => '1.2.3.4', // -m
-    'cephx_name' => 'admin' // -n
+    'cephx_name' => 'admin', // -n
     'cephx_keyfile' => '/path/to/keyfile', // -K
     'cephx_keyring' => '/path/to/keyring.bin', // -k
-
 );
 
 $r = new Rados();
