c9df88f29da7235a32ce327b5cbd42ec7be7f702
Wido den Hollander <wido@widodh.nl>
2012-07-04 12:13:55 +0200

    Partially revert merging the stream wrappers
    Those are far from ready and should never have been merged in

--- php_rados.h
+++ php_rados.h
@@ -20,9 +20,6 @@
 #define PHP_RADOS_CLUSTER_RES_NAME "RADOS Cluster"
 #define PHP_RADOS_IOCTX_RES_NAME "RADOS IOCtx"
 
-// We will register our streams as rados://
-#define PHP_RADOS_STREAM_WRAPPER "rados"
-
 typedef struct _php_rados_cluster {
     rados_t cluster;
 } php_rados_cluster;
@@ -73,21 +70,5 @@ PHP_FUNCTION(rados_ioctx_snap_get_stamp);
 
 extern zend_module_entry rados_module_entry;
 #define phpext_rados_ptr &rados_module_entry;
-
-static php_stream* rados_wrapper_open_url(php_stream_wrapper *wrapper, char *path, char *mode, int options, char **opened_path, php_stream_context *context STREAMS_DC TSRMLS_DC);
-static php_stream* rados_wrapper_open_dir(php_stream_wrapper *wrapper, char *filename, char *mode, int options, char **opened_path, php_stream_context *context STREAMS_DC TSRMLS_DC);
-static int rados_wrapper_rename(php_stream_wrapper *wrapper, char *url_from, char *url_to, int options, php_stream_context *context TSRMLS_DC);
-static int rados_wrapper_unlink(php_stream_wrapper *wrapper, char *url, int options, php_stream_context *context TSRMLS_DC);
-static int rados_wrapper_stat(php_stream_wrapper *wrapper, char *url, int flags, php_stream_statbuf *ssb, php_stream_context *context TSRMLS_DC);
-static int rados_wrapper_mkdir(php_stream_wrapper *wrapper, char *url, int mode, int options, php_stream_context *context TSRMLS_DC);
-static int rados_wrapper_rmdir(php_stream_wrapper *wrapper, char *url, int options, php_stream_context *context TSRMLS_DC);
-
-static size_t rados_stream_write(php_stream *stream, const char *buf, size_t count TSRMLS_DC);
-static size_t rados_stream_read( php_stream *stream, char *buf, size_t count TSRMLS_DC);
-static int    rados_stream_close(php_stream *stream, int close_handle TSRMLS_DC);
-static int    rados_stream_flush(php_stream *stream TSRMLS_DC);
-static int    rados_stream_seek( php_stream *stream, off_t offset, int whence, off_t *newoffset TSRMLS_DC);
-static int    rados_stream_stat( php_stream *stream, php_stream_statbuf *ssb TSRMLS_DC);
-
 
 #endif
